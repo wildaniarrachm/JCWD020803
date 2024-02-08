c9766f861c2aae9726770b888451621e6a732dd0
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-02-08 09:24:33 +0700

    Update Index.jsx

--- apps/web/src/pages/user-dashboard/profle-detail-page/verification-code/Index.jsx
+++ apps/web/src/pages/user-dashboard/profle-detail-page/verification-code/Index.jsx
@@ -1,5 +1,5 @@
 import { Layout } from '../../../../components/customers/Index';
-import { VerifyCode } from '../../../../components/customers/user-profile/profile-detail/verifyCode';
+import { VerifyCode } from '../../../../components/customers/user-profile/profile-detail/VerifyCode';
 
 function VerifyCodePages() {
   return (
